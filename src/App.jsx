54925bc5e10e4862ba0fdc6aaea2787a315c8dee
Gio <sagarcia612@gmail.com>
2025-09-15 14:16:14 +0200

    timer is good

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HandleCLick from "./components/handleClick";
 function App() {
   return (
     <>
-      <h1 class="bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent ">
+      <h1 className="bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent ">
         POMODORO APP
       </h1>
       <HandleCLick />
--- src/components/handleClick.jsx
+++ src/components/handleClick.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
 const HandleCLick = () => {
+  
   const [timer, setTimer] = useState(1500);
-
-  const handleTheClick = () => {
-    const minutes = timer.setMinutes(25);
+  
+  const handleTheClick = () => {    
+    setInterval(() => {
+      setTimer ((prev) => prev - 1)
+    }, 1000);
+   
   };
 
   return (
-    <>
-      <h1> </h1>
-    </>
+    <div>
+      <h1>{timer}</h1>
+      <button onClick={handleTheClick}> Start </button>
+    </div>
   );
 };
 
