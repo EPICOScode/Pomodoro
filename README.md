c02549eddf0bcb9f1ee411c952ca98fde4d24a35
Gio <sagarcia612@gmail.com>
2026-01-09 18:16:37 +0100

    Readme updated

---
+++ README.md
@@ -0,0 +1,11 @@
+    This App has been made as my very first complete app and it works as a portfolio  
+
+    The main idea is to have an enviroment for studying with the Pomodoro tecnique. 
+
+    Study blocks of 25 minutes and breaks of 5 minutes, trying to do a minimalistic but non bugged site to be fully concentrated as much as possible. 
+    As I develop the app we'll see that it is basic but it holds it's purpose entirely. 
+
+
+    The technologies I use here are React (Vite), and CSS( Tailwind, daysiUI ). 
+
+    I'll try to make it easy to understand for whomever wants to work on the code, and to implement some new tools little by little 
--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,56 @@ import Timer from "./components/timer";
 import Timerdisplay from "./components/timerDisplay";
 
 function App() {
-  const clock = 10; //The const for the time
+  const clock = 10;
+  const breakTime = 300;
 
-  const breakTime = 300; // The const for the break
+  const [time, setTime] = useState(clock);
+  const [run, setRun] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [theBreak, setTheBreak] = useState(false);
 
-  const [time, setTime] = useState(clock); // useState to setup the clock
-  const [run, setRun] = useState(false); //false = No state changes. This useState runs the useEffect when it's launched by the user
-  const [open, setOpen] = useState(false); // The state that opens the modal, headleeUI component
-  const [ theBreak, setTheBreak] = useState(false); 
-
-  
   /**********************    user actions   ***********************************/
 
   const startTimer = () => setRun(true); //function  => start tracking the time, =>  true because false makes the time stay the same
   const endTimer = () => setRun(false);
   const resetTimer = () => {
     setRun(false); // this line makes the code stop when it resets whithout "reset" button being pressed
-     if (theBreak) {
-       setTime(breakTime);
-    }else {
-      setTime (clock); 
-     }
-     // This returns to the very first time.
-  }; 
+    if (theBreak) {
+      setTime(breakTime);
+    } else {
+      setTime(clock);
+    }
+    // This returns to the very first time.
+  };
 
   const skipBreak = () => {
-    setTheBreak (false);
+    setTheBreak(false);
     setTime(clock);
-    setRun(true); 
-    setOpen(false)
-  } 
+    setRun(true);
+    setOpen(false);
+  };
 
-  const startBreak =() => {
-    setTheBreak(true); 
-    setTime(breakTime); 
-    setRun(false); 
-    setOpen(false); 
-  }
+  const startBreak = () => {
+    setTheBreak(true);
+    setTime(breakTime);
+    setRun(false);
+    setOpen(false);
+  };
 
   /******************************* Engine ************************************/
   useEffect(() => {
     if (!run) return; // Do nothing if not touched, it means that if this is not in the top of the clock  gear (useEffect), the counter will go on by itself
     const interval = setInterval(() => {
       // Creates the const where we keep the setInterval
-        setTime((prev) => {
-          if (prev <= 1) { // if the prop is less or equal to one then 
-            setRun(false), setOpen(true); // stop the effect of+ counting and open the modal 
-            return 0; // the clock stays in zero while the modal remains open 
-          } else {
-            return prev - 1; // Otherwise count backwards 
-          }},          
-      );
+      setTime((prev) => {
+        if (prev <= 1) {
+          // if the prop is less or equal to one then
+          setRun(false), setOpen(true); // stop the effect of+ counting and open the modal
+          return 0; // the clock stays in zero while the modal remains open
+        } else {
+          return prev - 1; // Otherwise count backwards
+        }
+      });
 
       // we can acces to the state, we create a function prev and then we make it go backwards using prev -1;
     }, 1000); // If I add brakets, it'll be an array and react is expecting numbers, instead just return the number  , 1000); not [1000];
@@ -65,18 +64,11 @@ function App() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="title">POMODORO</h1>
 
-      <Timerdisplay time={time} break={theBreak}/>
-
+      <Timerdisplay time={time} break={theBreak} />
 
       <HandleCLick start={startTimer} stop={endTimer} reset={resetTimer} />
 
-      
-      <Timer
-        open={open}
-        onBreak={startBreak}
-        onSkipBreak = {skipBreak}
-      />
-      
+      <Timer open={open} onBreak={startBreak} onSkipBreak={skipBreak} />
     </div>
   );
 }
